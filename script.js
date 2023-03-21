f67fbff83605ea0691a9ea92685df114826760f5
gustavwiese <gusse12345@gmail.com>
2023-03-21 15:39:50 +0100

    objects

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
 "use strict";
-console.log("script.js running🥳");
 
-const title = document.createElement("h1");
-title.textContent = "Hej!";
-document.querySelector("body").appendChild(title)
+const harry = {
+  firstName: "Harry",
+  lastName: "Potter",
+};
+
+const ron = {
+  firstName: "Ron",
+  lastName: "Weasly",
+};
+
+const hermione = {
+  firstName: "Hermione",
+  lastName: "Granger",
+};
+
+const tobias = {
+  firstName: "Tobias",
+  middleName: '"Fantastic Kisser"',
+  lastName: "Rasmussen",
+};
+
+function showStudent(student) {
+  document.querySelector("#firstName").textContent = student.firstName;
+  document.querySelector("#middleName").textContent = student.middleName;
+  document.querySelector("#lastName").textContent = student.lastName;
+}
+
+harry.middleName = "James";
+ron.middleName = "Biluius";
+hermione.middleName = "Jean";
+
+function addStudent(student) {
+  const liste = document.createElement("li");
+  liste.textContent = student.firstName + " " + student.middleName + " " + student.lastName;
+
+  document.querySelector("#students").appendChild(liste);
+}
